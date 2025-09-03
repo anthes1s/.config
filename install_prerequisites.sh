11559c329865e0074b2d468a3c4779dbc824943b
anthes1s <4nthesis@gmail.com>
2025-09-03 19:16:55 +0300

    install needed stuff script

---
+++ install_prerequisites.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+sudo pacman -Syu hyprland uwsm hyprpaper power-profiles-daemon docker docker-buildx docker-compose git firefox waybar brightnessctl alacritty neovim gcc make ripgrep fd unzip nodejs-lts-jod npm
