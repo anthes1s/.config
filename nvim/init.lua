f96f2913189aff963ea010ae783b4c280ee99d64
anthes1s <4nthesis@gmail.com>
2026-01-31 16:15:10 +0200

    sync

--- nvim/init.lua
+++ nvim/init.lua
@@ -919,6 +919,16 @@ require('lazy').setup({
     --    - Show your current context: https://github.com/nvim-treesitter/nvim-treesitter-context
     --    - Treesitter + textobjects: https://github.com/nvim-treesitter/nvim-treesitter-textobjects
   },
+  {
+    'f-person/git-blame.nvim',
+    event = 'VeryLazy',
+    opts = {
+      enabled = true,
+      message_template = ' <summary> | <date> | <author> | <<sha>>',
+      date_format = '%m-%d-%Y %H:%M:%S',
+      virtual_text_column = 1,
+    },
+  },
 
   -- The following comments only work if you have downloaded the kickstart repo, not just copy pasted the
   -- init.lua. If you want these files, they are in the repository, so you can just download them and
